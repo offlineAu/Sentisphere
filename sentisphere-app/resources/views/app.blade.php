6f9c2a1f6300a733e361e66b2d36c68aa62d3036
offlineAu <janoplo.airljoriz@gmail.com>
2025-12-03 14:08:13 +0800

    Update app.blade.php

--- sentisphere-app/resources/views/app.blade.php
+++ sentisphere-app/resources/views/app.blade.php
@@ -23,7 +23,6 @@
 
         <title inertia>{{ config('app.name', 'Sentisphere') }}</title>
 
-        <link rel="icon" href="/logo.ico" sizes="any">
         <link rel="icon" href="/logo.svg" type="image/svg+xml">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
